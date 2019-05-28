27461e9e2bfefa4ad2e3be1e84a715999fcff657
THCKwarter <MattsMisc.Items@yahoo.com>
2019-05-28 11:15:16 -0500

    Ch7 Overhaul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProductItemComponent } from './product-item/product-item.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { SearchComponent } from './search/search.component';
-import { StarsComponent } from './stars/stars.component';
-import { ProductService } from './shared/product.service';
-import { AuctionMaterialModule } from './auction-material/auction-material.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CarouselComponent,
-    FooterComponent,
-    NavbarComponent,
-    ProductItemComponent,
-    ProductDetailComponent,
-    SearchComponent,
-    StarsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    AuctionMaterialModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [
-    ProductService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
