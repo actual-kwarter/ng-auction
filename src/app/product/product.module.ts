decef0c7b4f12f6be2f372e5c2cce5bc37c3db25
THCKwarter <MattsMisc.Items@yahoo.com>
2019-05-30 17:29:46 -0500

    p222

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductComponent } from './product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductSuggestionComponent } from './product-suggestion/product-suggestion.component';
+
+@NgModule({
+  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
+  imports: [
+    CommonModule,
+    FlexLayoutModule
+  ]
+})
+export class ProductModule { }
